b6fd0582ff8dfb137b0f1010e70259d476beade6
Antonio Sucre <ajs2104@gmail.com>
2020-04-21 13:24:13 -0300

    feat: Adding Substraction, Multiplication and Division

--- RESTWithASPNETCoreUdemy 01 - Calculator/RESTWithASPNETCoreUdemy/Controllers/CalculatorController.cs
+++ RESTWithASPNETCoreUdemy 01 - Calculator/RESTWithASPNETCoreUdemy/Controllers/CalculatorController.cs
@@ -19,12 +19,45 @@ namespace RESTWithASPNETCoreUdemy.Controllers
             _logger = logger;
         }
 
-        [HttpGet("{firstNumber}/{secondNumber}")]
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
         public IActionResult Sum(string firstNumber, string secondNumber)
         {
             if (int.TryParse(firstNumber, out _) && int.TryParse(secondNumber, out _))
             {
                 var sum = Convert.ToDecimal(firstNumber) + Convert.ToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("subtraction/{firstNumber}/{secondNumber}")]
+        public IActionResult Subtraction(string firstNumber, string secondNumber)
+        {
+            if (int.TryParse(firstNumber, out _) && int.TryParse(secondNumber, out _))
+            {
+                var sum = Convert.ToDecimal(firstNumber) - Convert.ToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("Multiplication/{firstNumber}/{secondNumber}")]
+        public IActionResult Multiplication(string firstNumber, string secondNumber)
+        {
+            if (int.TryParse(firstNumber, out _) && int.TryParse(secondNumber, out _))
+            {
+                var sum = Convert.ToDecimal(firstNumber) * Convert.ToDecimal(secondNumber);
+                return Ok(sum.ToString());
+            }
+            return BadRequest("Invalid Input");
+        }
+
+        [HttpGet("Division/{firstNumber}/{secondNumber}")]
+        public IActionResult Division(string firstNumber, string secondNumber)
+        {
+            if (int.TryParse(firstNumber, out _) && int.TryParse(secondNumber, out _))
+            {
+                var sum = Convert.ToDecimal(firstNumber) / Convert.ToDecimal(secondNumber);
                 return Ok(sum.ToString());
             }
             return BadRequest("Invalid Input");
